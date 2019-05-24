c4165f1cbd4abf7bf632e46ff57968e9326b8397
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-24 02:13:50 -0700

    simple_lmk: Introduce Simple Low Memory Killer for Android
    This is a complete low memory killer solution for Android that is small
    and simple. Processes are killed according to the priorities that
    Android gives them, so that the least important processes are always
    killed first. Processes are killed until memory deficits are satisfied,
    as observed from kswapd struggling to free up pages. Simple LMK stops
    killing processes when kswapd finally goes back to sleep.
    
    The only tunables are the desired amount of memory to be freed per
    reclaim event and desired frequency of reclaim events. Simple LMK tries
    to free at least the desired amount of memory per reclaim and waits
    until all of its victims' memory is freed before proceeding to kill more
    processes.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -47,6 +47,7 @@
 #include <linux/prefetch.h>
 #include <linux/printk.h>
 #include <linux/dax.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -3314,7 +3315,12 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 		unsigned long nr_reclaimed = sc.nr_reclaimed;
 		bool raise_priority = true;
 
+<<<<<<< HEAD
 		sc.reclaim_idx = classzone_idx;
+=======
+		simple_lmk_decide_reclaim(sc.priority);
+		sc.nr_reclaimed = 0;
+>>>>>>> ceada99652479... simple_lmk: Introduce Simple Low Memory Killer for Android
 
 		/*
 		 * If the number of buffer_heads exceeds the maximum allowed
@@ -3437,6 +3443,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 
 	prepare_to_wait(&pgdat->kswapd_wait, &wait, TASK_INTERRUPTIBLE);
 
+<<<<<<< HEAD
 	/*
 	 * Try to sleep for a short interval. Note that kcompactd will only be
 	 * woken if it is possible to sleep for a short interval. This is
@@ -3445,6 +3452,12 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 * succeed.
 	 */
 	if (prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+=======
+	/* Try to sleep for a short interval */
+	if (prepare_kswapd_sleep(pgdat, order, remaining,
+						balanced_classzone_idx)) {
+		simple_lmk_stop_reclaim();
+>>>>>>> ceada99652479... simple_lmk: Introduce Simple Low Memory Killer for Android
 		/*
 		 * Compaction records what page blocks it recently failed to
 		 * isolate pages from and skips them in the future scanning.
@@ -3479,8 +3492,14 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 * After a short sleep, check if it was a premature sleep. If not, then
 	 * go fully to sleep until explicitly woken up.
 	 */
+<<<<<<< HEAD
 	if (!remaining &&
 	    prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+=======
+	if (prepare_kswapd_sleep(pgdat, order, remaining,
+						balanced_classzone_idx)) {
+		simple_lmk_stop_reclaim();
+>>>>>>> ceada99652479... simple_lmk: Introduce Simple Low Memory Killer for Android
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
